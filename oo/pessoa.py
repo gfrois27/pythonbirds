c68156374fb3e6eff3eef9ccd43bdd1ff2152b00
guilherme <guilhermefrois15@yahoo.com.br>
2022-06-01 00:07:41 -0300

    criado metado cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,11 @@
-class Pessoa :
-    pass
+class Pessoa:
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
 
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
 
